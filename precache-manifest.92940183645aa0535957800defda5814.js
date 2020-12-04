76d0edc465e320c1a1e191d464271758a0562466
Nas <nashamproj@gmail.com>
2020-12-04 16:04:19 +0000

    Updates

---
+++ precache-manifest.92940183645aa0535957800defda5814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7567032f33f917e3e24a3c9a9cf002df",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "937b63647b088ce37966",
+    "url": "/mycv/static/css/main.2265fb8c.chunk.css"
+  },
+  {
+    "revision": "644d3ffa5a5437261938",
+    "url": "/mycv/static/js/2.b6a7bfc4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mycv/static/js/2.b6a7bfc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937b63647b088ce37966",
+    "url": "/mycv/static/js/main.bcb69f42.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  },
+  {
+    "revision": "2f5c8f47011a0cb757815c1d6b245395",
+    "url": "/mycv/static/media/initial-rune.2f5c8f47.png"
+  }
+]);
